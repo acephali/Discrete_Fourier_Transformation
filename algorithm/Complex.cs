715f3a51a0d9c8a5819ed0281139f955bb1345d0
acephali <embru99@gmail.com>
2024-02-22 14:57:06 +0100

    Adding files and updating README

---
+++ algorithm/Complex.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace algorithm
+{
+    internal class Complex
+    {
+        public double real;
+        public double imaginary;
+
+        public Complex(double real, double imaginary)
+        {
+            this.real = real;
+            this.imaginary = imaginary;
+        }
+
+        public Complex(int degree)
+        {
+            double radian = degree * Math.PI / 180;
+            real = Math.Round(Math.Cos(radian), 2);
+            imaginary = Math.Round(Math.Sin(radian), 2);
+        }
+
+        public static Complex operator +(Complex a, Complex b) => new Complex(a.real + b.real, a.imaginary + b.imaginary);
+
+        public static Complex operator -(Complex a, Complex b) => new Complex(a.real - b.real, a.imaginary - b.imaginary);
+
+        public static Complex operator *(Complex a, Complex b) => new Complex(a.real * b.real - a.imaginary * b.imaginary, a.real * b.imaginary + a.imaginary * b.real);
+
+        public override string ToString()
+        {
+            return $"{real} {(imaginary < 0 ? "-" : "+")} {Math.Abs(imaginary)}j";
+        }
+    }
+}
---
+++ algorithm/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace algorithm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int N = 3;
+            //Complex[] f = new Complex[N] { new Complex(52 / 4, 0), new Complex(-28 / 4, 0), new Complex(12 / 4, 0), new Complex(32 / 4, 0) }; // example for 4 elements
+            Complex[] f = new Complex[N] { new Complex(-81 / 3, 0), new Complex(27 / 3, 0), new Complex(3 / 3, 0) };
+            Complex[] F = new Complex[N];
+
+            List<int> k_l_szorzatok = new List<int>();
+            List<Complex> W = new List<Complex>();
+            Dictionary<int, Complex> W_dict = new Dictionary<int, Complex>();
+
+            for (int i = 0; i < f.Length; i++)
+            {
+                for (int j = 0; j < F.Length; j++)
+                {
+                    if (!k_l_szorzatok.Contains(-i * j))
+                    {
+                        k_l_szorzatok.Add(-i * j);
+                    }
+                }
+            }
+
+            foreach (var item in k_l_szorzatok)
+            {
+                int degree = 360 / f.Length * item;
+                W.Add(new Complex(degree));
+            }
+
+            for (int i = 0; i < k_l_szorzatok.Count; i++)
+            {
+                W_dict.Add(k_l_szorzatok[i], W[i]);
+            }
+
+            for (int i = 0; i < F.Length; i++)
+            {
+                Complex szorzatOsszeg = new Complex(0, 0);
+                for (int j = 0; j < f.Length; j++)
+                {
+                    int szorzat = -i * j;
+                    Complex szorzando = W_dict[szorzat];
+                    szorzatOsszeg += f[j] * szorzando;
+                }
+                F[i] = szorzatOsszeg;
+            }
+
+            foreach (var item in F)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
